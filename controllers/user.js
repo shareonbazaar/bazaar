be9fec7a738c7a6f8f2eeed33d73e21295a2d7af
Rory MacQueen <rorymacqueen@gmail.com>
2015-09-20 12:35:56 -0300

    Remove website field for user model

--- controllers/user.js
+++ controllers/user.js
@@ -144,7 +144,6 @@ exports.postUpdateProfile = (req, res, next) => {
     user.profile.name = req.body.name || '';
     user.profile.gender = req.body.gender || '';
     user.profile.location = req.body.location || '';
-    user.profile.website = req.body.website || '';
     user.save((err) => {
       if (err) {
         if (err.code === 11000) {
