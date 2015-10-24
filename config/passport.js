e9456a93e827956928e6bb326aafd18c73666337
Rory MacQueen <rorymacqueen@gmail.com>
2015-10-24 19:19:52 -0200

    Redirect to new account page after sign in

--- config/passport.js
+++ config/passport.js
@@ -91,7 +91,7 @@ passport.use(new FacebookStrategy(configuration, function(req, accessToken, refr
           req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Facebook manually from Account Settings.' });
           done(err);
         } else {
-          req.session.returnTo = '/account';
+          req.session.returnTo = '/newaccount';
           var user = new User();
           user.email = profile._json.email;
           user.facebook = profile.id;
