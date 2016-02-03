f43b5cdcf6fcf5412508348438ff321c371a51f0
Rory MacQueen <rorymacqueen@gmail.com>
2016-02-03 14:55:43 -0200

    Fix JS file names
    Reinstate the old main.js code we had.
    Move splash code to splash.js

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,96 +1,96 @@
 /*
-    Alpha by HTML5 UP
+    Spectral by HTML5 UP
     html5up.net | @n33co
     Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-    skel.breakpoints({
-        wide: '(max-width: 1680px)',
-        normal: '(max-width: 1280px)',
-        narrow: '(max-width: 980px)',
-        narrower: '(max-width: 840px)',
-        mobile: '(max-width: 736px)',
-        mobilep: '(max-width: 480px)'
-    });
+    skel
+        .breakpoints({
+            xlarge: '(max-width: 1680px)',
+            large:  '(max-width: 1280px)',
+            medium: '(max-width: 980px)',
+            small:  '(max-width: 736px)',
+            xsmall: '(max-width: 480px)'
+        });
 
     $(function() {
 
-        var    $window = $(window),
+        var $window = $(window),
             $body = $('body'),
-            $header = $('#header'),
-            $banner = $('#banner');
+            $wrapper = $('#page-wrapper'),
+            $banner = $('#banner'),
+            $header = $('#header');
+
+        // Disable animations/transitions until the page has loaded.
+            $body.addClass('is-loading');
+
+            $window.on('load', function() {
+                window.setTimeout(function() {
+                    $body.removeClass('is-loading');
+                }, 100);
+            });
+
+        // Mobile?
+            if (skel.vars.mobile)
+                $body.addClass('is-mobile');
+            else
+                skel
+                    .on('-medium !medium', function() {
+                        $body.removeClass('is-mobile');
+                    })
+                    .on('+medium', function() {
+                        $body.addClass('is-mobile');
+                    });
 
         // Fix: Placeholder polyfill.
             $('form').placeholder();
 
-        // Prioritize "important" elements on narrower.
-            skel.on('+narrower -narrower', function() {
+        // Prioritize "important" elements on medium.
+            skel.on('+medium -medium', function() {
                 $.prioritize(
-                    '.important\\28 narrower\\29',
-                    skel.breakpoint('narrower').active
+                    '.important\\28 medium\\29',
+                    skel.breakpoint('medium').active
                 );
             });
 
-        // Dropdowns.
-            $('#nav > ul').dropotron({
-                alignment: 'right'
-            });
+        // Scrolly.
+            $('.scrolly')
+                .scrolly({
+                    speed: 1500,
+                    offset: $header.outerHeight()
+                });
 
-        // Off-Canvas Navigation.
-
-            // Navigation Button.
-                $(
-                    '<div id="navButton">' +
-                        '<a href="#navPanel" class="toggle"></a>' +
-                    '</div>'
-                )
-                    .appendTo($body);
-
-            // Navigation Panel.
-                $(
-                    '<div id="navPanel">' +
-                        '<nav>' +
-                            $('#nav').navList() +
-                        '</nav>' +
-                    '</div>'
-                )
-                    .appendTo($body)
-                    .panel({
-                        delay: 500,
-                        hideOnClick: true,
-                        hideOnSwipe: true,
-                        resetScroll: true,
-                        resetForms: true,
-                        side: 'left',
-                        target: $body,
-                        visibleClass: 'navPanel-visible'
-                    });
-
-            // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-                if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-                    $('#navButton, #navPanel, #page-wrapper')
-                        .css('transition', 'none');
+        // Menu.
+            $('#menu')
+                .append('<a href="#menu" class="close"></a>')
+                .appendTo($body)
+                .panel({
+                    delay: 500,
+                    hideOnClick: true,
+                    hideOnSwipe: true,
+                    resetScroll: true,
+                    resetForms: true,
+                    side: 'right',
+                    target: $body,
+                    visibleClass: 'is-menu-visible'
+                });
 
         // Header.
-        // If the header is using "alt" styling and #banner is present, use scrollwatch
-        // to revert it back to normal styling once the user scrolls past the banner.
-        // Note: This is disabled on mobile devices.
-            if (!skel.vars.mobile
-            &&    $header.hasClass('alt')
-            &&    $banner.length > 0) {
+            if (skel.vars.IEVersion < 9)
+                $header.removeClass('alt');
 
-                $window.on('load', function() {
+            if ($banner.length > 0
+            &&  $header.hasClass('alt')) {
 
-                    $banner.scrollwatch({
-                        delay:        0,
-                        range:        0.5,
-                        anchor:        'top',
-                        on:            function() { $header.addClass('alt reveal'); },
-                        off:        function() { $header.removeClass('alt'); }
-                    });
+                $window.on('resize', function() { $window.trigger('scroll'); });
 
+                $banner.scrollex({
+                    bottom:     $header.outerHeight() + 1,
+                    terminate:  function() { $header.removeClass('alt'); },
+                    enter:      function() { $header.addClass('alt'); },
+                    leave:      function() { $header.removeClass('alt'); }
                 });
 
             }
