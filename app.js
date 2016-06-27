3956f4506741652b6a7d591a04098e43675392cb
Rory MacQueen <rorymacqueen@gmail.com>
2016-06-27 16:24:31 -0500

    Fix fallout from rebase

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const expressValidator = require('express-validator');
 const sass = require('node-sass-middleware');
-const multer = require('multer');
-const upload = multer({ dest: path.join(__dirname, 'uploads') });
+const cookieParser = require('cookie-parser');
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -72,7 +71,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(methodOverride());
 app.use(cookieParser());
 var store = new MongoStore({ url: secrets.db, autoReconnect: true });
 app.use(session({
@@ -86,9 +84,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-var multer  = require('multer')
-var storage = multer.memoryStorage()
-var upload = multer({ storage: storage })
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 app.use(upload.single('profilepic'));
 
 app.use(lusca({
